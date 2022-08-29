8fff835bd84ef96f57476554da1dff7d88f8290f
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 <damian.koncewicz@wp.pl>
2022-08-29 13:06:00 +0200

    loops

---
+++ src/clojureproject/Loop.clj
@@ -0,0 +1,43 @@
+(ns clojureproject.Loop)
+
+(defn Loop
+  []
+  (println "LOOP:")
+  (loop [x 0]
+    (when (< x 10)
+      (println x)
+      (recur (inc x)
+             )
+      )
+    )
+  )
+(Loop)
+
+(defn DoTimes
+  []
+  (println "Do Times:")
+  (dotimes [x 10]
+    (println x)
+    )
+  )
+(DoTimes)
+
+(defn WhileDo
+  [count]
+  (println "While Do")
+  (def x (atom 0))
+  (while (< @x count)
+    (do
+      (println @x)
+      (swap! x inc)))
+  )
+(WhileDo 10)
+
+(defn DoSeq
+  [seq]
+  (println "DO SeQ")
+  (doseq [x seq]
+    (println (inc x))
+    )
+  )
+(DoSeq [1 2 3])
