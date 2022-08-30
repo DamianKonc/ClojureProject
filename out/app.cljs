9bea264ad4d117334c6b9e4064e4a00313e8a2fe
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 <damian.koncewicz@wp.pl>
2022-08-30 12:23:01 +0200

    first clojurescript line

---
+++ out/app.cljs
@@ -0,0 +1,23 @@
+#_{:clj-kondo/ignore [:namespace-name-mismatch]}
+(ns app
+  (:gen-class))
+
+(js/console.log "Hello World!")
+(js/console.log "Cos innego")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ out/app.js
@@ -0,0 +1,7 @@
+// Compiled by ClojureScript 1.11.60 {:optimizations :none}
+goog.provide('app');
+goog.require('cljs.core');
+console.log("Hello World!");
+console.log("Cos innego");
+
+//# sourceMappingURL=app.js.map
