cab1708b45c1a9f47304de47a0a3e0eaa2dd213d
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 <damian.koncewicz@wp.pl>
2022-08-30 16:03:05 +0200

    cleaning

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to clojureproject
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- out/app.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.11.60 {:optimizations :none}
-goog.provide('app');
-goog.require('cljs.core');
-console.log("Hello World!");
-console.log("Cos innego");
-
-//# sourceMappingURL=app.js.map
