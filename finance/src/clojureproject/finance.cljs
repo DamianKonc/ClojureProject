ef1c6ed64cf656dc25616dda25eb763fd853d421
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 <damian.koncewicz@wp.pl>
2022-09-01 19:12:25 +0200

    navbar

--- finance/src/clojureproject/finance.cljs
+++ finance/src/clojureproject/finance.cljs
@@ -14,10 +14,24 @@
 (defn get-app-element []
   (gdom/getElement "app"))
 
+(defn navigation []
+  [:nav {:class "navigation"}
+   [:ul {:class "navigation__list"}
+    [:li {:class "navigation__list_el"} "Menu1"]
+    [:li {:class "navigation__list_el"} "Menu2"]
+    [:li {:class "navigation__list_el"} "Menu3"]
+    [:li {:class "navigation__list_el"} "Menu4"]]])
+
 (defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/clojureproject/finance.cljs and watch it change!"]])
+  [:div {:class "wrapper"}
+   [:div {:class "halo"}
+    [:h1  {:class "header"} 
+     [:a {:class "header__logo" :href "./"} (:text @app-state)]
+    ]
+    [navigation]
+    ]
+   
+   [:p "Edit this in src/clojureproject/finance.cljs and watch it change!"]])
 
 (defn mount [el]
   (rdom/render [hello-world] el))
@@ -25,15 +39,8 @@
 (defn mount-app-element []
   (when-let [el (get-app-element)]
     (mount el)))
-
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
 (mount-app-element)
 
-;; specify reload hook with ^:after-load metadata
 (defn ^:after-load on-reload []
   (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
