48a851beeffeef1cc6f15f38d210d95186163aa6
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 <damian.koncewicz@wp.pl>
2022-09-01 12:59:11 +0200

    first steps to make weather app

--- weather/src/learn_cljs/weather.cljs
+++ weather/src/learn_cljs/weather.cljs
@@ -1,59 +1,45 @@
 (ns ^:figwheel-hooks learn-cljs.weather
-  (:require
-   [goog.dom :as gdom]
-   [reagent.core :as reagent :refer [atom]]
-   [reagent.dom :as rdom]))
+    (:require
+     [goog.dom :as gdom]
+     [reagent.dom :as rdom]
+     [reagent.core :as r]))
 
-(println "This text is printed from src/learn_cljs/weather.cljs. Go ahead and edit it and see reloading in action.")
+(defonce app-state (r/atom {:title "WhichWeather"
+                            :postal-code ""
+                            :temperatures {:today {:label "Today"
+                                                   :value nil}
+                                           :tomorrow {:label "Tomorrow"
+                                                      :value nil}}}))
 
-(defn multiply [a b] (* a b))
+(defn  title[]
+  [:h1(:title @app-state)])
 
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
-(defonce greeter (atom {:text "Hello Man"}))
+(defn temperature [temp]
+  [:div{:class "temperature"}
+   [:div {:class "value"}
+    (:value temp)]
+   [:h2 (:label temp)]])
 
-(defn get-app-element []
-  (gdom/getElement "app"))
+(defn postal-code []
+  [:div {:class "postal-code"}
+   [:h3 "Enter your postal code"]
+   [:input {:type "text"
+            :placeholder "Postal Code"
+            :value (:postal-code @app-state)}]
+   [:button "Go"]])
+
+(defn app []
+  [:div {:class "app"}
+   [title]
+   [:div {:class "temperatures"}
+    (for [temp (vals (:temperatures @app-state))]
+      [temperature temp])]
+   [postal-code]])
 
 
-(def input (.createElement js/document "input"))           ;; <2>
-;; #'learn-cljs.weather/input                              ;; <3>
 
-(.appendChild (.-body js/document) input)
-;; #object[HTMLInputElement [object HTMLInputElement]]
+(defn mount-app-element []                                 ;; <4>
+  (rdom/render [app] (gdom/getElement "app")))
 
-(set! (.-placeholder input) "Enter something")             ;; <4>
-;; "Enter something"
-
-(defn handle-input [e]                                     ;; <5>
-  (swap! app-state assoc :text (-> e .-target .-value)))
-;; #'learn-cljs.weather/handle-input
-
-(set! (.-onkeyup input) handle-input)
-;; #object[learn_cljs$weather$handle_input ...]
-
-(defn hello-world []
-  [:div
-  
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/learn_cljs/weather.cljs and watch it change!"]
-    [:p (:text @greeter)]])
-
-(defn mount [el]
-  (rdom/render [hello-world] el))
-
-(defn mount-app-element []
-  (when-let [el (get-app-element)]
-    (mount el)))
-
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
-(mount-app-element)
-
-;; specify reload hook with ^:after-load metadata
-(defn ^:after-load on-reload []
-  (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn ^:after-load on-reload []                            ;; <4>
+  (mount-app-element))
